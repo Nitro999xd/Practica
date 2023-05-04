3e08086080f8cd579677a63cbd196641c5b581bf
alumno <alumno@DAM1-12>
2023-05-04 13:47:12 +0200

    1

---
+++ src/david/Alvarez_David.java
@@ -0,0 +1,9 @@
+package david;
+
+public class Alvarez_David {
+
+public static void main (String[] args) {
+	System.out.println("1");
+	
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author alumno
+ *
+ */
+module Alvarez_David {
+}
